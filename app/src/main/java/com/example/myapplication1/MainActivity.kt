975ad0eae9c06a15eeb3e084474180229c783fe5
Brook1711 <33785776+Brook1711@users.noreply.github.com>
2020-05-14 16:56:44 +0800

    can encode text to morse sound and can judge dit and dot by sound

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,22 +1,135 @@
 package com.example.myapplication1
 
+//import com.
+
+import android.content.Intent
+import android.util.Log
+import android.Manifest
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import be.tarsos.dsp.AudioEvent
+
+import be.tarsos.dsp.GainProcessor
+
+import be.tarsos.dsp.io.android.AudioDispatcherFactory
+import be.tarsos.dsp.pitch.PitchDetectionHandler
+import be.tarsos.dsp.pitch.PitchDetectionResult
+import be.tarsos.dsp.pitch.PitchProcessor
 import com.google.android.material.textfield.TextInputEditText
-//import com.
-import com.google.android.material.textfield.TextInputLayout
-import kotlin.concurrent.thread
+
+import java.util.LinkedList
+
+
+import com.yashovardhan99.timeit.Stopwatch
+import com.yashovardhan99.timeit.Split
+
 
 class MainActivity : AppCompatActivity() {
-
+    var pitch_detection_isRunning = true
+    var last_low_sys_time:Long = 0//低电平的开头
+    var last_high_sys_time:Long = 0//高电平的开头
+    var last_state:Int = 0 //0 -> . 1 -> -
+    var temp_decode_morse:String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //使用activity_main.xml文件作为程序主界面
+
+
+        val REQUEST_EXTERNAL_STORAGE = 1
+        val PERMISSIONS_STORAGE = arrayOf<String>(
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        if (PackageManager.PERMISSION_GRANTED !=
+            ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.WRITE_CONTACTS)
+        ) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE)
+        }
+
+        val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0)
+        val pdh = PitchDetectionHandler{ res, e ->
+            val pitchInHz = res.pitch
+            runOnUiThread{processPitch(pitchInHz, pitch_detection_isRunning)}
+        }
+//
+        val pitchProcessor = PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN,22050f,1024,pdh)
+        //val pitchProcessor1 = PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_PITCH,22050f,1024,pdh)
+        //val gainProcessor = GainProcessor(0.0)
+        dispatcher.addAudioProcessor(pitchProcessor)
+        val audioThread = Thread(dispatcher, "Audio Thread")
+        audioThread.start()
+
+        //val stopwatch = Stopwatch()
+        //stopwatch.setTextView(findViewById(R.id.stop_time))
     }
+
+    fun processPitch(pitchInHz:Float, pitch_detection_isRunning:Boolean){
+        var tv:TextView = findViewById<TextView>(R.id.frequence)
+        var tv_decode_result = findViewById<TextView>(R.id.decode_result)
+
+        var current_sys_time:Long = System.currentTimeMillis()
+        if(pitch_detection_isRunning){
+            tv.text = pitchInHz.toString()
+            tv_decode_result.text = temp_decode_morse
+            if (pitchInHz < 1100 && pitchInHz > 900){//当前低电平是line或dit（高电平）
+                if(last_state == 0){//这是一个上升沿
+                    var Low_duratinog_t_ms = current_sys_time - last_low_sys_time
+                    if (Low_duratinog_t_ms > 1000){//被认可的摩斯码符号间隔
+                        this.last_high_sys_time = current_sys_time//更新系统状态
+                        this.last_state = 1
+                        temp_decode_morse = temp_decode_morse.plus('/')//更新摩斯码
+                        println(temp_decode_morse)
+                    }
+                    else if (Low_duratinog_t_ms > 50)//码字间隔
+                    {
+                        this.last_state = 1
+                        this.last_high_sys_time = current_sys_time
+                    }
+                    else{//不被接受的符号间隔和码字间隔对系统没有影响
+
+                    }
+                }
+                else{//处于高电平当中
+                }
+            }
+            else{//当前电平是低电平
+                if (this.last_state == 1){//这是一个下降沿
+                    var High_duration_t_ms = current_sys_time -last_high_sys_time
+                    if (High_duration_t_ms >50){//被认可的摩斯码符号脉冲
+                        this.last_low_sys_time = current_sys_time//更新
+                        this.last_state = 0
+                        if (High_duration_t_ms > 200){//判断符号类型
+                            temp_decode_morse = temp_decode_morse.plus('-')
+                        }
+                        else{
+                            temp_decode_morse = temp_decode_morse.plus('.')
+                        }
+                    }
+                    else{//不被接受的摩斯码脉冲
+
+                    }
+                }
+                else{//处于低电平当中
+
+                }
+            }
+
+        }
+        else{
+            tv_decode_result.text = ""
+        }
+
+    }
+
+
     fun clickHandler(source: View)
     {
         //获取UI界面中ID为R.id.show的文本框
@@ -58,7 +171,7 @@ class MainActivity : AppCompatActivity() {
                 '.' -> {
                     mPlayer_dit.start()
                     Thread.currentThread()
-                    Thread.sleep(300)
+                    Thread.sleep(400)
                 }
                 '-' -> {
                     mPlayer_line.start()
@@ -67,9 +180,16 @@ class MainActivity : AppCompatActivity() {
                 }
                 '/' -> {
                     Thread.currentThread()
-                    Thread.sleep(500)
+                    Thread.sleep(1000)
                 }
             }
         }
+        mPlayer_dit.stop()
+        mPlayer_line.stop()
+    }
+
+    fun clickHandler_decode(view: View) {
+//val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(2048, 1024, 0)
+        pitch_detection_isRunning = pitch_detection_isRunning == false
     }
 }
